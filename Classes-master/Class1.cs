37516f9320d4bc6f186c78acd7d9b26745abc8bd
Дарья Лавренюк <114358972+fatfreakdasha@users.noreply.github.com>
2023-04-06 10:58:03 +0300

    Add files via upload

---
+++ Classes-master/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Students
+    {
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private int BaseClasses { get; set; }
+        public Students(string name, string surname, int baseclasses)
+        {
+            Name = name;
+            Surname = surname;
+            BaseClasses = baseclasses;
+        }
+        public override string ToString()
+        {
+            return "имя студента: " + Name + ". фамилия студента: " + Surname + ". поступил на базе " + BaseClasses + " классов ";
+        }
+    }
+}
---
+++ Classes-master/Program.cs
@@ -0,0 +1,17 @@
+﻿using Classes;
+Students[] students = new Students[5];
+for (int i = 0; i < students.Length; i++)
+{
+    Console.WriteLine("введите имя студента:");
+    string name = Console.ReadLine();
+    Console.WriteLine("введите фамилию студента:");
+    string surname = Console.ReadLine();
+    Console.WriteLine("введите базу классов студента:");
+    int baseclasses = Convert.ToInt32(Console.ReadLine());
+    students[i] = new Students(name, surname, baseclasses);
+    Console.WriteLine("\n");
+}
+for (int i = 0; i < students.Length; i++)
+{
+    Console.WriteLine(students[i]);
+}
